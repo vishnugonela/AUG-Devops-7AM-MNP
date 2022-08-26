2c71f62a61636e6012844571df6a9c27de5bce68
Sravani <sravanikona94@gmail.com>
2022-08-26 10:18:43 +0000

    Script to print file content

---
+++ linux/filecontent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to print file content 
+
+read -p " Enter file name :  " name 
+
+echo -e " Number of lines are :\c " ; cat $name | wc -l  
+echo -e " Number of words are : \c " ; cat $name | wc -w
+echo -e " Number of charecters are : \c " ; cat $name | wc -m
+
