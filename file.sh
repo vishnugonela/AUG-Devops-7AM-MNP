fcd76e95777e017b018338c3d9b0f3dcd937d6fe
root <root@ip-172-31-24-120.ec2.internal>
2022-08-28 06:10:15 +0000

    created a new file

---
+++ file.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -e "enter your server name \c "; uname
+echo -e "enter your memory utilization \c "; free -h
+echo -e "enter your disk utilization \c "; df -h
+echo -e "enter your IP address \c"; hostname -i
