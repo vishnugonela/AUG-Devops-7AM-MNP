d8af0cdd242ec816edf9b1762e75c04149c3091a
skona5 <111981479+skona5@users.noreply.github.com>
2022-08-29 09:01:40 +0530

    Script to print content of the file

--- linux/filecontent.sh
+++ linux/filecontent.sh
@@ -9,3 +9,5 @@ echo -e " Number of words are : \c " ; cat $name | wc -w
 echo -e " Number of charecters are : \c " ; cat $name | wc -m
 
 # echo -e " Number of lines are :\c " ; wc -l $name | awk '{print $1}'
+
+
