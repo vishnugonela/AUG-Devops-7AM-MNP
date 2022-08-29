86926588a8ae0c0a967185d88b0b43982fd04155
skona5 <111981479+skona5@users.noreply.github.com>
2022-08-29 11:28:10 +0530

    Update string.sh
    Script to loop based on number of characters in a string

--- linux/string.sh
+++ linux/string.sh
@@ -3,14 +3,13 @@
 # Script for string with loop 
 
 read -p " Enter your name : " name
-
 len=${#name} 
 echo $len
 
 for (( i=0; i<$len; i++ )) 
 	
 do
-	echo " $(($i+1)) Charecter is ${name:$i:(1)} "
+	echo " $(($i+1)) Charecter is ${name:$i:1} "
 
 done
 
